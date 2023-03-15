74473f2e2cd1353734ecfe5830ba7e7244ff5233
Sulkipli507 <sulkiply507@gmail.com>
2023-03-15 23:57:34 +0800

    update pagination

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -32,14 +32,32 @@
 	<div class="mobile-menu-overlay"></div>
 
     <div class="main-container">
-		
-		<div class="pd-30 bg-white border-radius-4 box-shadow mb-30">
-            @yield('content')
-		</div>
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				{{-- <div class="page-header">
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="title">
+								<h4>blank</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">blank</li>
+								</ol>
+							</nav>
+						</div>
+					</div>
+				</div> --}}
+				<div class="pd-30 bg-white border-radius-4 box-shadow mb-30">
+                    @yield('content')
+				</div>
+			</div>
 
             {{-- Footer --}}
-        @include('backend.partial.footer')
+            @include('backend.partial.footer')
             {{-- End Footer --}}
+		</div>
 	</div>
 
 
