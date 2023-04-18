c8ccd718e0c74b690142111fabebc2335aeacac6
Sulkipli507 <sulkiply507@gmail.com>
2023-04-18 18:16:20 +0800

    update dashboard home

--- resources/views/admin/dashboard/home.blade.php
+++ resources/views/admin/dashboard/home.blade.php
@@ -1,6 +1,29 @@
 @extends('backend.master')
 @section('content')
-    <h2>Jumlah Ruangan: {{ $countRoom }} </h2>
-    <h2>Jumlah Pengguna: {{ $countUser }} </h2>
-    <h2>Jumlah Peminjaman: {{ $countLoan }} </h2>
+    <div class="row">
+        <div class="col-sm-4">
+          <div class="card text-white bg-secondary">
+            <div class="card-body text-center">
+              <h5 class="card-title">Jumlah Ruangan</h5>
+              <h1>{{ $countRoom }}</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card text-dark bg-warning">
+            <div class="card-body text-center">
+              <h5 class="card-title">Jumlah Pengguna</h5>
+              <h1>{{ $countUser }}</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card text-dark bg-info">
+            <div class="card-body text-center">
+              <h5 class="card-title">Jumlah Peminjaman</h5>
+              <h1>{{ $countLoan }}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
